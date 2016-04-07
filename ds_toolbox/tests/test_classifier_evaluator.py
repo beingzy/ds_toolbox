515bdee85b428c743cacd72a3dd7b97ab48d52a1
beingzy <beingzy@gmail.com>
2016-04-07 15:37:07 -0400

    initiate unit test files

---
+++ ds_toolbox/tests/test_classifier_evaluator.py
@@ -0,0 +1,15 @@
+import unittest
+from ds_toolbox.model_selection_functions import classifier_evaluator
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
