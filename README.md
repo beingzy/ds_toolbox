39c92be32f3f39b1f0ad61935fb1e0a00942e0d1
beingzy <beingzy@gmail.com>
2016-04-25 15:15:49 -0400

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### DS Toolbox (Data Scienec Toolbox)
 The python library aims to provide high-level functions to help data scientist
-develop prototype models quickly and easily. 
+develop prototype models quickly and easily.
 
 
 ### How to use:
@@ -13,7 +13,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 
-from ds_toolbox.model_selection_functions import model_selection_cv 
+from ds_toolbox.model_selection_functions import model_selection_cv
 from ds_toolbox.model_selection_functions import classifier_evaluator
 from ds_toolbox.model_selection_functions import eval_barchart
 
@@ -24,14 +24,37 @@ RF = RandomForestClassifier()
 SVM = SVC( probability=True)
 
 # package classification algorithms in dictionary
-classifiers = {"Logistic Regression (L1: Lasso)": LogisticModel, 
+classifiers = {"Logistic Regression (L1: Lasso)": LogisticModel,
                "SVM (Kernel=RDF)": SVM,
-               "Boosting Tree": Boosting, 
+               "Boosting Tree": Boosting,
                "Random Forest": RF}
-               
-# evaluation model with stratified k-fold cross-validation 
+
+# evaluation model with stratified k-fold cross-validation
 # cross-validation results were exported as pandas.DataFrames.
 train_reports, test_reports = model_selection_cv(classifiers, xdata, ydata,
-                                      k=10, random_state=_RANDOM_STATE, 
+                                      k=10, random_state=_RANDOM_STATE,
                                       eval_func = classifier_evaluator )
+```
+
+
+2. model evaluation
+```{python}
+from ds_toolbox.model_selection_functions import classifier_evaluator
+eval_classier_report = classifier_evaluator(clf, xx, yy)
+```
+
+3. create balanced sample data from imbalanced data
+```{python}
+from ds_toolbox.Sampler import balanced_sample_maker
+xx, yy = balanced_sample_maker(xx, yy)
+```
+
+4. create model ensembler
+```{python}
+from ds_toolbox.Ensembler import WeightingAverageEnsembler
+
+ensemlber = WeightingAverageEnsembler()
+ensemlber.load_models([LR, BOOST, DT], weights=[0.3, 0.5, 0.2])
+pred_labels = ensembler.predict(xx)
+pred_probs = ensembler.predict_proba(xx)
 ```
--- ds_toolbox/model_selection_functions.py
+++ ds_toolbox/model_selection_functions.py
@@ -8,7 +8,7 @@ import pandas as pd
 from pandas import DataFrame
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.metrics import roc_curve, auc, confusion_matrix, f1_score
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
@@ -42,16 +42,16 @@ def classifier_evaluator(model, x, y):
            "sensitivity": sensitivity, "specificity": specificity,
            "precision": precision}
     return res
-    
+
 
 def model_selection_cv( models, x, y, k=5, eval_func=None, random_state=None):
-    """ framework for model selection based on stratified 
+    """ framework for model selection based on stratified
         cross-validation
-    
+
         Parameters:
         ----------
         * models: {dictionary}, key: model label, value: learner object
-        * x: {np.array}, predictor data 
+        * x: {np.array}, predictor data
         * y: {np.array}, response variable data
         * k: {integer}, the number of folds in cross-validation
         * random_state: {integer}, the random state set for replication
@@ -60,8 +60,8 @@ def model_selection_cv( models, x, y, k=5, eval_func=None, random_state=None):
     # stratified cross_validation
     cv = StratifiedKFold( y, n_folds=k, shuffle=False, random_state=random_state)
     tot_models = len( models.keys() )
-    tot_iter = tot_models * k 
-    
+    tot_iter = tot_models * k
+
     pbar = tqdm(total=tot_iter)
 
     train_reports, test_reports = [], []
@@ -76,19 +76,19 @@ def model_selection_cv( models, x, y, k=5, eval_func=None, random_state=None):
             # retrieve data for relevant usage
             x_train, y_train = x[train_idx], y[train_idx]
             x_test, y_test = x[test_idx], y[test_idx]
-            # training model 
+            # training model
             model.fit( x_train, y_train )
             # evaluation model
             train_score = eval_func( model, x_train, y_train )
             train_score["model_name"] = model_name
             test_score = eval_func( model, x_test, y_test )
             test_score["model_name"] = model_name
-            
+
             train_reports.append( train_score )
             test_reports.append( test_score )
-            
+
             pbar.update()
-            
+
     pbar.close()
 
     # convert list of performance records into dataframe
@@ -103,39 +103,39 @@ def model_selection_cv( models, x, y, k=5, eval_func=None, random_state=None):
     test_reports = test_reports[["model_name"] + metrics_names]
 
     return train_reports, test_reports
-    
-    
-def eval_barchart(df, 
-                  test_metrics_colnames, 
+
+
+def eval_barchart(df,
+                  test_metrics_colnames,
                   train_metrics_colnames,
-                  label_colname = None, 
+                  label_colname = None,
                   ylabel = 'Performance Score' ):
-    """ plot model evaluation performance 
-    
+    """ plot model evaluation performance
+
         Parameters:
         ----------
         * df: {pandas.DataFrame}
-        * label_colname: {string}, the column name for colum keeping model name 
+        * label_colname: {string}, the column name for colum keeping model name
         * test_metrics_colnames: {list}, columns for test metric mean, columns for test metric std.
         * train_metrics_colnames: {list}, columns for train metric mean, columns for train metric std.
-        * ylabel: {string} y-axis label 
-    """    
+        * ylabel: {string} y-axis label
+    """
     if ( label_colname==None ):
         label_colname = df.columns[0]
-    
+
     labels = df[ label_colname ].tolist()
-    
+
     test_mean = df[ test_metrics_colnames[0] ].tolist()
     test_std  = df[ test_metrics_colnames[1] ].tolist()
-    
+
     train_mean = df[ train_metrics_colnames[0] ].tolist()
     train_std  = df[ train_metrics_colnames[1] ].tolist()
-    
+
     # graphic parameter
     N = len(labels)
     ind = np.arange(N)
     width = 0.35 # the width of the bars
-    
+
     fig, ax = plt.subplots()
     # plot 1st test error
     rects1 = ax.bar(ind, test_mean, width, color='g', yerr = test_std)
@@ -154,12 +154,12 @@ def eval_barchart(df,
         # attach some text labels
         for rect in rects:
             height = rect.get_height()
-            ax.text(rect.get_x() + rect.get_width() / 2., 
+            ax.text(rect.get_x() + rect.get_width() / 2.,
                     1.05*height,
                     '%d' % int(height),
                      ha='center', va='bottom')
-    
+
     autolabel( rects1 )
     autolabel( rects2 )
-    
-    return fig 
+
+    return fig
